47dd91f0507a50fc765b26a07ab93a290fd17b14
AlaaElzoghby <AlaaElzoghby@Alaa>
2020-12-06 23:33:06 +0200

    Creating ProductCatalog model in data layer

---
+++ OA.Data/Models/ProductCatalog.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OA.Data.Models
+{
+    public class ProductCatalog
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string photoName { get; set; }
+        public decimal price { get; set; }
+        public DateTime lastUpdated { get; set; }
+
+    }
+}
