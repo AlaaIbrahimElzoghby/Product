cb5e108feb6046bfa7c77a7fb402bf608995dfe8
AlaaElzoghby <AlaaElzoghby@Alaa>
2020-12-07 00:48:57 +0200

    Adding preoduct repository class to repository later

---
+++ OA.Repo/Repositories/BaseRepository.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OA.Repo.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace OA.Repo.Repositories
+{
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
+    {
+
+        #region Fields
+        protected ApplicationContext _context;
+        protected readonly DbSet<TEntity> _entities;
+        #endregion
+
+        #region Constructor
+        public BaseRepository(ApplicationContext context)
+        {
+            _context = context;
+            _entities = _context.Set<TEntity>();
+        }
+        #endregion
+
+        #region CRUD
+        public void Insert(TEntity entity)
+        {
+            try
+            {
+                _entities.Add(entity);
+
+            }
+            catch (Exception e)
+            {
+                // we can log exception here in exception table
+            }
+
+        }
+        public void Update(TEntity entity)
+        {
+            try
+            {
+                _context.Entry(entity).State = EntityState.Modified;
+            }
+            catch (Exception e)
+            {
+                // we can log exception here in exception table
+            }
+        }
+        public void Delete(TEntity entity)
+        {
+            try
+            {
+                _entities.Remove(entity);
+            }
+            catch (Exception e)
+            {
+                // we can log exception here in exception table
+            }
+        }
+        #endregion
+
+        #region Queries
+        public TEntity GetById(long id)
+        {
+            try
+            {
+                return _entities.Find(id);
+            }
+            catch (Exception e)
+            {
+                // we can log exception here in exception table
+                return null;
+            }
+        }
+        public IEnumerable<TEntity> GetAll()
+        {
+            try
+            {
+                return _entities.AsEnumerable();
+            }
+            catch (Exception e)
+            {
+                // we can log exception here in exception table
+                return null;
+            }
+        }
+        public IEnumerable<TEntity> FindBy(Expression<Func<TEntity, bool>> predicate)
+        {
+            try
+            {
+                return _entities.Where(predicate);
+            }
+            catch (Exception e)
+            {
+                // we can log exception here in exception table
+                return null;
+            }
+        }
+        #endregion
+
+
+        public bool SaveChanges()
+        {
+            try
+            {
+                return _context.SaveChanges() > 0;
+            }
+            catch (Exception e)
+            {
+                // we can log exception here in exception table
+                return false;
+            }
+
+        }
+
+    }
+
+}
---
+++ OA.Repo/Repositories/ProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using OA.Repo.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OA.Repo.Repositories
+{
+    public class ProductRepository : BaseRepository<OA.Data.ProductCatalog>, IProductRepository
+    {
+        #region Fields
+        private readonly ApplicationContext _applicationContext;
+        #endregion
+
+        #region Constructores
+        public ProductRepository(ApplicationContext context) : base(context)
+        {
+            _context = context;
+        }
+        #endregion
+
+        #region ComplexQueries
+        
+
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+
+}
