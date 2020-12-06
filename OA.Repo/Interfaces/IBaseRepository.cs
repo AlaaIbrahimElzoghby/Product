0d39a83953972b00629cb8c7827d6d3eb91ee170
AlaaElzoghby <AlaaElzoghby@Alaa>
2020-12-07 00:33:17 +0200

    Adding product interfaces in repository layer

---
+++ OA.Repo/Interfaces/IBaseRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace OA.Repo.Interfaces
+{
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        #region CRUD
+        void Insert(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity enitity);
+        #endregion
+
+        #region Queries
+        TEntity GetById(long id);
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> FindBy(Expression<Func<TEntity, bool>> predicate);
+        #endregion
+
+        bool SaveChanges();
+    }
+}
---
+++ OA.Repo/Interfaces/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OA.Repo.Interfaces
+{
+    public interface IProductRepository : IBaseRepository<OA.Data.ProductCatalog>
+    {
+        #region ComplexQueries
+        
+        #endregion
+
+
+        #region Methods
+        
+        #endregion
+    }
+}
