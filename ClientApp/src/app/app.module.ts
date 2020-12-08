7302cac0939710bc92fd8bd0227cde6c769cdd84
AlaaElzoghby <AlaaElzoghby@Alaa>
2020-12-08 11:18:41 +0200

    Adding product components

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http'
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { ProductService } from './product.service';
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductAddEditComponent } from './product-add-edit/product-add-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ProductAddEditComponent
   ],
   imports: [
     BrowserModule
